b752164217c44890f76fda45a96268fcbd143a96
skp544 <shubhamkumarprajapati544@gmail.com>
2025-11-22 13:28:50 +0530

    feat: add Welcome component with GSAP animations and text hover effects, and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "#components/Navbar";
+import Welcome from "#components/Welcome";
 import React from "react";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Welcome />
     </main>
   );
 };
